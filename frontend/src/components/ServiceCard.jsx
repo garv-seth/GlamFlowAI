c3ec5583fc7dcc6eaa08b5536627134c42cfd070
JACQUELINE <jacquelinesmith190@gmail.com>
2024-05-11 15:03:24 -0700

    change colours of services card

--- frontend/src/components/ServiceCard.jsx
+++ frontend/src/components/ServiceCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   CardHeader,
   CardFooter,
@@ -8,48 +7,72 @@ import {
   Button,
   Heading,
   Text,
-  Link
+  Link,
+  useColorModeValue,
 } from '@chakra-ui/react';
 
-export default function CaptionCarousel() {
+export default function ServiceCard() {
+  const cardBg = useColorModeValue('background', 'gray.900');
+  const cardTextColor = useColorModeValue('text', 'text');
+  const buttonBg = useColorModeValue('button.bg', 'button.bg');
+  const buttonText = useColorModeValue('button.text', 'button.text');
+  const buttonHoverBg = useColorModeValue('button.hoverBg', 'button.hoverBg');
 
   return (
     <SimpleGrid spacing={2} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-  <Card>
-    <CardHeader>
-      <Heading size='md'> Hair Salons</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-    <Link to="/salon-details">
-          <Button>View here</Button>
-        </Link>
-    </CardFooter>
-  </Card>
-  <Card>
-    <CardHeader>
-      <Heading size='md'> Tattoo Parlours</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
-  <Card>
-    <CardHeader>
-      <Heading size='md'>Nail Salons</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
-</SimpleGrid>
+      <Card bg={cardBg} color={cardTextColor}>
+        <CardHeader>
+          <Heading size='md'>Hair Salons</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>View a summary of all your customers over the last month.</Text>
+        </CardBody>
+        <CardFooter>
+          <Link href="/salon-details">
+            <Button
+              bg={buttonBg}
+              color={buttonText}
+              _hover={{ bg: buttonHoverBg }}
+            >
+              View here
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+      <Card bg={cardBg} color={cardTextColor}>
+        <CardHeader>
+          <Heading size='md'>Tattoo Parlours</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>View a summary of all your customers over the last month.</Text>
+        </CardBody>
+        <CardFooter>
+          <Button
+            bg={buttonBg}
+            color={buttonText}
+            _hover={{ bg: buttonHoverBg }}
+          >
+            View here
+          </Button>
+        </CardFooter>
+      </Card>
+      <Card bg={cardBg} color={cardTextColor}>
+        <CardHeader>
+          <Heading size='md'>Nail Salons</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>View a summary of all your customers over the last month.</Text>
+        </CardBody>
+        <CardFooter>
+          <Button
+            bg={buttonBg}
+            color={buttonText}
+            _hover={{ bg: buttonHoverBg }}
+          >
+            View here
+          </Button>
+        </CardFooter>
+      </Card>
+    </SimpleGrid>
   );
 }
